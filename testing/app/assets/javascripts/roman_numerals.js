e4b7d920e22e780fe1b6ce22a379a69a6c104dce
Patrick Vine <pvine@prodigyfinance.com>
2016-09-06 12:14:35 +0200

    beginning of JS tests for roman numerals

--- testing/app/assets/javascripts/roman_numerals.js
+++ testing/app/assets/javascripts/roman_numerals.js
@@ -2,6 +2,18 @@ var RomanNumerals = function() {
   var self = this;
 
   self.convert = function(number) {
-    return 'I';
+    mapping = [
+      {'number': 10, 'symbol': 'X'},
+      {'number': 5, 'symbol': 'V'},
+      {'number': 4, 'symbol': 'IV'},
+      {'number': 1, 'symbol':  'I'}
+    ];
+    for(var i = 0; i < mapping.length; i++) {
+      roman_mapping = mapping[i];
+      if(number >= roman_mapping['number']) {
+        return roman_mapping['symbol'] + this.convert(number - roman_mapping['number']);
+      }
+    }
+    return '';
   }
 }
--- testing/spec/javascripts/roman_numberals_spec.js
+++ testing/spec/javascripts/roman_numberals_spec.js
@@ -5,8 +5,38 @@ describe("roman numberals kata", function() {
     expect(convertor.convert(1)).toBe('I');
   });
 
-/*  it("2 to return II", function() {*/
-    //convertor = new RomanNumerals();
-    //expect(convertor.convert(2)).toBe('II');
-  /*});*/
+  it("2 to return II", function() {
+    convertor = new RomanNumerals();
+    expect(convertor.convert(2)).toBe('II');
+  });
+
+  it("4 to return IV", function() {
+    convertor = new RomanNumerals();
+    expect(convertor.convert(4)).toBe('IV');
+  });
+
+  it("5 to return V", function() {
+    convertor = new RomanNumerals();
+    expect(convertor.convert(5)).toBe('V');
+  });
+
+  it("6 to return VI", function() {
+    convertor = new RomanNumerals();
+    expect(convertor.convert(5)).toBe('V');
+  });
+
+  it("10 to return X", function() {
+    convertor = new RomanNumerals();
+    expect(convertor.convert(10)).toBe('X');
+  });
+
+  it("20 to return XX", function() {
+    convertor = new RomanNumerals();
+    expect(convertor.convert(20)).toBe('XX');
+  });
+
+  it("33 to return XX", function() {
+    convertor = new RomanNumerals();
+    expect(convertor.convert(33)).toBe('XXXIII');
+  });
 });
